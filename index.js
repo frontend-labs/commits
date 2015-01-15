4bcb3057fd2c35fdd19bbbf3e878a68f24997b3d
Jan Sanchez <joejansanchez@gmail.com>
2015-01-15 15:24:53 -0500

    feat(index.js): agrego index.js para los demos

---
+++ index.js
@@ -0,0 +1,19 @@
+/*!!
+ * 
+ * index.js
+ * @author: Jan Sanchez
+ *
+ */
+
+var fn = {
+    sumar: function(a, b){
+        return a + b;
+    },
+    restar: function(){
+        return a - b;
+    },
+    multiplicar: function(){
+        return a * b;
+    }
+}
+
